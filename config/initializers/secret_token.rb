ce49449bff39fb510f335893f2f7a1c1fb5637df
Abhishek <abhisheksunkuru@gmail.com>
2013-07-31 17:39:54 +0530

    angular js app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularCasts::Application.config.secret_token = 'd511a8b2280fe9d1c29fbf361043eb7a09cb8eb555376cfdb82b7e45d222dde81c0861d912bfd30c465fed4016339ac1c07a225e69df0c9294666f8e3073b433'
